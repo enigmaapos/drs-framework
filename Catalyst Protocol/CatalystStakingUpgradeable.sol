abde88293a783e1e44f29dd8c2ed1fba0c360eb6
enigmaapos <apostoliaenigma@gmail.com>
2025-10-06 10:40:59 +0800

    Update CatalystStakingUpgradeable.sol

--- Catalyst Protocol/CatalystStakingUpgradeable.sol
+++ Catalyst Protocol/CatalystStakingUpgradeable.sol
@@ -984,7 +984,7 @@ function _applyTaxAndSplit(address user, uint256 amount, address collection) int
 
 function _getTopBurners(uint256 topCount) internal view returns (address[] memory topBurners) {
     uint256 n = participatingWallets.length;
-    if (n == 0) return new address (0);
+    if (n == 0) return new address[](0);
     if (n > MAX_PARTICIPANTS_LIMIT) revert TooManyParticipants();
 
     // Copy to temporary arrays for sorting (safe under limit)
