8854e18b3a86f3cbdc1c37e685205790831a50aa
enigmaapos <apostoliaenigma@gmail.com>
2025-10-07 21:15:01 +0800

    Create TestNFT.sol

---
+++ Catalyst Protocol/TestNFT.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// ✅ Import OpenZeppelin ERC721 standard
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestNFT is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
+    string private _baseTokenURI;
+
+    constructor(string memory baseURI) ERC721("TestNFT", "TNFT") {
+        _baseTokenURI = baseURI;
+    }
+
+    /// @notice Mint new NFT to `to` address
+    function mintNFT(address to) external onlyOwner {
+        _tokenIdCounter++;
+        _safeMint(to, _tokenIdCounter);
+    }
+
+    /// @notice Batch mint NFTs
+    function mintBatch(address to, uint256 amount) external onlyOwner {
+        for (uint256 i = 0; i < amount; i++) {
+            _tokenIdCounter++;
+            _safeMint(to, _tokenIdCounter);
+        }
+    }
+
+    /// @notice View total supply minted so far
+    function totalMinted() external view returns (uint256) {
+        return _tokenIdCounter;
+    }
+
+    /// @notice Set new base token URI (optional)
+    function setBaseURI(string memory newURI) external onlyOwner {
+        _baseTokenURI = newURI;
+    }
+
+    /// @dev Override baseURI for metadata
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+}
